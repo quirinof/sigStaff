606e5b4ff41108bb7f462bad1533a1f3607d1fcf
quirinof <matheusquinoff@gmail.com>
2023-03-13 20:13:18 -0300

    iniciando esboço da tela principal

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main(void) {
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||           Universidade Federal do Rio Grande do Norte          ||\n");
+    printf(" ||               Centro de Ensino Superior do Serido              ||\n");
+    printf(" ||            Departamento de Computacao e Tecnologia             ||\n");
+    printf(" ||                   Disciplina -- Programacao                    ||\n");
+    printf(" ||               Projeto de Sistema de Funcionarios               ||\n");
+    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023        ||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||              ====== Sistema de Funcionários ======             ||\n");
+    printf(" ||                                                                ||\n");
+    return 0;
+}
--- projeto.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> 
-int main(void) {
-    printf("            Universidade Federal do Rio Grande do Norte \n");
-    return 0;
-}

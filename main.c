f0a39975978cdd39d37f7a1f1666ff52b63eb281
quirinof <matheusquinoff@gmail.com>
2023-03-14 12:58:48 -0300

    esboço tela principal

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h> 
+
+void tela_principal(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
 int main(void) {
+    tela_principal();
+    return 0;
+}
+
+void tela_principal(void) {
+    char escolha;
     printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf(" ||                                                                ||\n");
     printf(" ||           Universidade Federal do Rio Grande do Norte          ||\n");
@@ -13,5 +24,14 @@ int main(void) {
     printf(" ||                                                                ||\n");
     printf(" ||        ====== Sistema de Controle de Funcionarios ======       ||\n");
     printf(" ||                                                                ||\n");
+    printf(" ||         [ 1 ] Funcionarios                                     ||\n");
+    printf(" ||         [ 2 ] Ponto                                            ||\n");
+    printf(" ||         [ 3 ] Folha de Pagamento                               ||\n");
+    printf(" ||         [ 0 ] Sair                                             ||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||          Digite sua escolha: ");
+    scanf("%c", &escolha);
+    printf(" ||                                                                ||\n");
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     return 0;
 }

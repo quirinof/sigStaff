8bb97768b0a05f2f5339302c84f94ddce32c84a1
quirinof <matheusquinoff@gmail.com>
2023-03-15 22:54:05 -0300

    inserindo a tela sobre

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ void tela_equipe(void);
 
 int main(void) {
     tela_principal();
+    tela_sobre();
     return 0;
 }
 
 void tela_principal(void) {
     char escolha;
+    system("clear||cls");
     printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf(" ||                                                                ||\n");
     printf(" ||           Universidade Federal do Rio Grande do Norte          ||\n");
@@ -27,11 +29,38 @@ void tela_principal(void) {
     printf(" ||         [ 1 ] Funcionarios                                     ||\n");
     printf(" ||         [ 2 ] Ponto                                            ||\n");
     printf(" ||         [ 3 ] Folha de Pagamento                               ||\n");
+    printf(" ||         [ 4 ]                                                  ||\n");
     printf(" ||         [ 0 ] Sair                                             ||\n");
     printf(" ||                                                                ||\n");
-    printf(" ||          Digite sua escolha: ");
+    printf(" ||         Digite sua escolha: ");
     scanf("%c", &escolha);
     printf(" ||                                                                ||\n");
     printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    return 0;
+    printf("\n");
+}
+
+void tela_sobre(void) {
+    system("clear||cls");
+    printf("\n");
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||           Universidade Federal do Rio Grande do Norte          ||\n");
+    printf(" ||               Centro de Ensino Superior do Serido              ||\n");
+    printf(" ||            Departamento de Computacao e Tecnologia             ||\n");
+    printf(" ||                   Disciplina -- Programacao                    ||\n");
+    printf(" ||          Projeto de Sistema de Controle de Funcionarios        ||\n");
+    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023        ||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||        ====== Sistema de Controle de Funcionarios ======       ||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||  Temos aqui um projeto da disciplina de programacao, que serve ||\n");
+    printf(" ||  para nos(alunos) sermos avaliados mediante os conhecimentos   ||\n");
+    printf(" ||  adquiridos durante o periodo. Contem um sistema para controle ||\n");
+    printf(" ||  de funcionarios, simulando as funcionalides necessarias para  ||\n");
+    printf(" ||  tal sistema. Criado apenas para fins didaticos.               ||\n");
+    printf(" ||                                                                ||\n");
+    printf(" ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf("\n");
 }

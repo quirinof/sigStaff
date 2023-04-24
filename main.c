b5d556644e82da87237db64f6c0be3d303f71783
quirinof <matheusquinoff@gmail.com>
2023-04-24 15:57:16 -0300

    telas sobre/equipe separadas

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "funcionarios.h"
 #include "projetos.h"
 #include "atividades.h"
+#include "sobre.h"
 
 char tela_principal(void);
 void tela_sobre(void);
@@ -26,60 +27,6 @@ int main(void) {
         }
     } while (opcao != '0');
     return 0;
-}
-
-void tela_sobre(void) {
-
-    system("cls||clear");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||           Universidade Federal do Rio Grande do Norte           ||\n");
-    printf(" ||               Centro de Ensino Superior do Serido               ||\n");
-    printf(" ||            Departamento de Computacao e Tecnologia              ||\n");
-    printf(" ||                   Disciplina -- Programacao                     ||\n");
-    printf(" ||          Projeto de Sistema de Controle de Funcionarios         ||\n");
-    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023         ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||        ====== Sistema de Controle de Funcionarios ======        ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||  Temos aqui um projeto da disciplina de programacao, que serve  ||\n");
-    printf(" ||  para nos(alunos) sermos avaliados mediante os conhecimentos    ||\n");
-    printf(" ||  adquiridos durante o periodo. Contem um sistema para controle  ||\n");
-    printf(" ||  de funcionarios de uma software house, simulando as            ||\n");
-    printf(" ||  funcionalides necessarias para tal sistema. Criado apenas      ||\n");
-    printf(" ||  para fins didaticos.                                           ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    getchar();
-}
-
-void tela_equipe(void) {
-
-    system("cls||clear");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||           Universidade Federal do Rio Grande do Norte           ||\n");
-    printf(" ||               Centro de Ensino Superior do Serido               ||\n");
-    printf(" ||            Departamento de Computacao e Tecnologia              ||\n");
-    printf(" ||                   Disciplina -- Programacao                     ||\n");
-    printf(" ||          Projeto de Sistema de Controle de Funcionarios         ||\n");
-    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023         ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||        ====== Sistema de Controle de Funcionarios ======        ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||               Este projeto foi desenvolvido por:                ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||                        Matheus Quirino                          ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" ||           * Email: matheusquinoff@gmail.com                     ||\n");
-    printf(" ||           * Git: https://github.com/quirinof                    ||\n");
-    printf(" ||                                                                 ||\n");
-    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    getchar();
 }
 
 char tela_principal(void) {
@@ -107,5 +54,6 @@ char tela_principal(void) {
     printf("       Digite o numero da sua escolha: ");
     scanf("%c", &escolha);
     getchar();
+
     return escolha;
 }
---
+++ sobre.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sobre.h"
+
+void tela_sobre(void) {
+
+    system("cls||clear");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||           Universidade Federal do Rio Grande do Norte           ||\n");
+    printf(" ||               Centro de Ensino Superior do Serido               ||\n");
+    printf(" ||            Departamento de Computacao e Tecnologia              ||\n");
+    printf(" ||                   Disciplina -- Programacao                     ||\n");
+    printf(" ||          Projeto de Sistema de Controle de Funcionarios         ||\n");
+    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023         ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||        ====== Sistema de Controle de Funcionarios ======        ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||  Temos aqui um projeto da disciplina de programacao, que serve  ||\n");
+    printf(" ||  para nos(alunos) sermos avaliados mediante os conhecimentos    ||\n");
+    printf(" ||  adquiridos durante o periodo. Contem um sistema para controle  ||\n");
+    printf(" ||  de funcionarios de uma software house, simulando as            ||\n");
+    printf(" ||  funcionalides necessarias para tal sistema. Criado apenas      ||\n");
+    printf(" ||  para fins didaticos.                                           ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    getchar();
+}
+
+void tela_equipe(void) {
+
+    system("cls||clear");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||           Universidade Federal do Rio Grande do Norte           ||\n");
+    printf(" ||               Centro de Ensino Superior do Serido               ||\n");
+    printf(" ||            Departamento de Computacao e Tecnologia              ||\n");
+    printf(" ||                   Disciplina -- Programacao                     ||\n");
+    printf(" ||          Projeto de Sistema de Controle de Funcionarios         ||\n");
+    printf(" ||         Desenvolvido por @quirinof -- desde marco, 2023         ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||        ====== Sistema de Controle de Funcionarios ======        ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||               Este projeto foi desenvolvido por:                ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||                        Matheus Quirino                          ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" ||           * Email: matheusquinoff@gmail.com                     ||\n");
+    printf(" ||           * Git: https://github.com/quirinof                    ||\n");
+    printf(" ||                                                                 ||\n");
+    printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    getchar();
+}

b97d446e9899f385570f511fc0fbef6c596cb7f8
Matheus Quirino <matheusquinoff@gmail.com>
2023-05-20 20:50:52 -0300

    atualizando validaçoes

--- validacoes.c
+++ validacoes.c
@@ -4,6 +4,7 @@
 #include "validacoes.h"
 
 
+
 int numero(char n) {
     if (n >= '0' && n <= '9') {
         return 1;
@@ -13,22 +14,32 @@ int numero(char n) {
     }
 }
 
-int valida_id(char* id, int tam) {
-    //       o id em si, o tamanho do id para tornar uma função genérica
+int quantidade_digitos(char* v, int x) {
     int i;
-
-    // quantidade de digitos
-    for (i = 0; i < tam; i++) {
-        if (id[i] == '\0') {
+    for (i = 0; i < x; i++) {
+        if (v[i] == '\0') {
             return 0;
         }
     }
-    if (id[i] != '\0') {
+    if (v[i] != '\0') {
         return 0;
     }
-    
+    else {
+        return 1;
+    }
+}
+
+int valida_id(char* id, int t) {
+    //       o id em si, o tamanho do id para tornar uma função genérica
+    int i;
+
+    // verifica a quantidade de digitos
+    if (!quantidade_digitos(id, t)) {
+        return 0;
+    }
+
     // apenas digitos numericos
-    for (i = 0; i < tam; i++) {
+    for (i = 0; i < t; i++) {
         if (!numero(id[i])) {
             return 0;
         }
@@ -39,20 +50,14 @@ int valida_id(char* id, int tam) {
 }
 
 
-
-int valida_cpf(char *cpf) {
+int valida_cpf(char* cpf) {
     int i, j;
     int d1 = 0;
     int d2 = 0;
     // verificação da quantidade de digitos
-    for (i = 0; i < 11; i++) {
-        if (cpf[i] == '\0') {
-            return 0;
-        }
-    }
-    if (cpf[i] != '\0') {
+    if (!quantidade_digitos(cpf, 11)) {
         return 0;
-    } 
+    }
     // verificação da existencia de apenas digitos numericos 
     for (i = 0; i < 11; i++) {
         if (!numero(cpf[i])) {
@@ -80,23 +85,64 @@ int valida_cpf(char *cpf) {
     return 1;
 }
 
+int bissexto(int ano) {
+    if ((ano % 4 == 0) && (ano % 100 != 0)) {
+        return 1;
+    }
+    else if (ano % 400 == 0) {
+        return 1;     
+    }
+    else {
+        return 0;
+    }
+}
 
-int valida_data(char *data) {
-    int i;
-    
+int ehdata(int dd, int mm, int aaaa) {
+    int max;
+    if (aa < 0 || mm < 1 || mm > 12 ) {
+        return 0;
+    }
+    if (mm == 2) {
+        if(bissexto(aa)) {
+            max = 29;
+        }
+        else {
+            max = 28;
+        }
+    } else if (mm == 4 || mm == 6 || mm == 9 || mm == 11) {
+        max = 30;
+    } 
+    else {
+        max = 31;
+    }
+    if (dd < 1 || dd > max) {
+        return 0;
+    }
+    return 1;
+} 
+
+int valida_data(char* data) {
+    int i, dia, mes, ano;
+    // eliminar as barras ou pontos que separam os dias, meses e anos
+    limpa_caracteres(data);
     // verifica quantidade de digitos 
-    for (i = 0; i < 10; i++) {
-        if (data[i] == '\0') {
+    if (!quantidade_digitos(data, 8)) {
+        return 0;
+    }
+    // se sao apenas numeros
+    for (i = 0; i < 8; i++) {
+        if (!numero(data[i])) {
             return 0;
         }
     }
-    if (data[i] != '\0') {
-        return 0;
-    }    
-    // verifica se as barras estao separando o dia/mes/ano
-    if (data[2] != '/' || data[5] != '/') {
+    dia = (data[0] - '0') * 10 + (data[1] - '0');
+    mes = (data[2] - '0') * 10 + (data[3] - '0');
+    ano = (data[4] - '0') * 1000 + (data[5] - '0') * 100 + (data[6] - '0') * 10 + (data [7] - '0');
+    // verifica se é uma data valida
+    if (!ehdata(dia, mes, ano)) {
         return 0;
     }
+    return 1;
 }
 
 int letra(char l) {
@@ -119,4 +165,15 @@ int valida_nome(char* nome) {
         }
     }
     return 1;
+}
+
+void limpa_caracteres(char* str) {
+    int i, j = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (numero(str[i])) {
+            str[j] = str[i];
+            j++;
+        }
+    }
+    str[j] = '\0';
 }
--- validacoes.h
+++ validacoes.h
@@ -1,9 +1,13 @@
-int valida_cpf(char *);
+int valida_cpf(char*);
 int valida_id(char*, int);
-int valida_data(char *);
+int valida_data(char*);
 int valida_nome(char*);
-int valida_email(char *);
+int valida_email(char*);
 int valida_cel(char*);
 
 int letra(char);
 int numero(char);
+int digitos(char*, int);
+int bissexto(int);
+int ehdata(int, int, int);
+void limpa_caracteres(char*);

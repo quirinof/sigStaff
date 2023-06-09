29f8230ee832d1784980a218151db1992839670e
quirinof <matheusquinoff@gmail.com>
2023-06-09 14:46:08 -0300

    Modulo Relatorios - Descartando a função de atv por data

--- relatorios.h
+++ relatorios.h
@@ -19,7 +19,6 @@ void listar_funcionarios(void);
 void listar_projetos(void);
 void listar_atividades(void);
 void listar_atv_filtrada(char*);
-void listar_atividades_por_data(char*);
 void listar_atividades_por_fnc(char*);
 void listar_atividades_por_pjt(char*);
 

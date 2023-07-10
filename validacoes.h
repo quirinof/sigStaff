335c5873b397586f329aa728b65ccec006d599bd
quirinof <matheusquinoff@gmail.com>
2023-07-10 14:32:28 -0300

    Assinaturas das verificações - Add descrição

--- validacoes.h
+++ validacoes.h
@@ -1,3 +1,4 @@
+// Funções que validam os campos necessarios para o sistema
 int valida_cpf(char*);
 int valida_id(char*, int);
 int valida_data(char*);
@@ -5,6 +6,8 @@ int valida_nome(char*);
 int valida_email(char*);
 int valida_cel(char*);
 
+
+// Funções que são uteis para algumas validações
 int letra(char);
 int numero(char);
 int quantidade_digitos(char*, int);
